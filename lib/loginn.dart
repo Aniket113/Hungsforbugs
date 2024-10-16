d82e1bca00c3392c6c079b0330ed59c037d3549b
aniketsalvi <aniketsalvi2745@gmail.com>
2024-10-16 15:44:29 +0530

    Updated GUI and UI of login , sign in page and all pages in student login

--- lib/loginn.dart
+++ lib/loginn.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
-import 'package:cool_alert/cool_alert.dart'; // Import CoolAlert package
+import 'package:cool_alert/cool_alert.dart';
 import 'package:placement1/company/company_home.dart';
 import 'package:placement1/student/home.dart';
-import 'package:placement1/signup.dart'; // Import your signup page
+import 'package:placement1/signup.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -36,9 +36,7 @@ class _LoginPageState extends State<LoginPage> {
             .get();
 
         if (studentSnapshot.docs.isNotEmpty) {
-          // Email and password match for student
           _showSuccessAlert('Login successful! Welcome Student!');
-          // Optionally navigate after the alert closes
           Future.delayed(Duration(seconds: 2), () {
             Navigator.pushReplacement(
               context,
@@ -46,9 +44,7 @@ class _LoginPageState extends State<LoginPage> {
             );
           });
         } else if (recruiterSnapshot.docs.isNotEmpty) {
-          // Email and password match for recruiter
           _showSuccessAlert('Login successful! Welcome Recruiter!');
-          // Optionally navigate after the alert closes
           Future.delayed(Duration(seconds: 2), () {
             Navigator.pushReplacement(
               context,
@@ -56,11 +52,9 @@ class _LoginPageState extends State<LoginPage> {
             );
           });
         } else {
-          // No match found, show error snackbar
           _showErrorSnackbar('Invalid credentials, check email or password');
         }
       } catch (e) {
-        // Handle errors
         print('Error during login: $e');
         _showErrorSnackbar('An error occurred, please try again');
       }
@@ -72,7 +66,7 @@ class _LoginPageState extends State<LoginPage> {
       context: context,
       type: CoolAlertType.success,
       text: message,
-      autoCloseDuration: const Duration(seconds: 2), // Auto close after 2 seconds
+      autoCloseDuration: const Duration(seconds: 2),
     );
   }
 
@@ -99,125 +93,133 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: true,
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Center(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Campus Placement App',
-                  style: TextStyle(
-                    color: Colors.purple,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 32.0),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Login ID',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Enter your email',
-                      hintStyle: TextStyle(color: Colors.grey),
-                      contentPadding: EdgeInsets.all(12),
-                    ),
-                    style: TextStyle(color: Colors.black),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your email';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      _email = value!;
-                    },
-                  ),
-                ),
-                SizedBox(height: 25.0),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Password',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Enter your password',
-                      hintStyle: TextStyle(color: Colors.grey),
-                      contentPadding: EdgeInsets.all(12),
-                    ),
-                    style: TextStyle(color: Colors.black),
-                    obscureText: true,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your password';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      _password = value!;
-                    },
-                  ),
-                ),
-                SizedBox(height: 32.0),
-                ElevatedButton(
-                  onPressed: _login,
-                  child: Text('Sign In'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blueGrey,
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      side: BorderSide(color: Colors.black),
-                    ),
-                    elevation: 5,
-                  ),
-                ),
-                SizedBox(height: 16.0), // Add space between button and register label
-                GestureDetector(
-                  onTap: () {
-                    // Navigate to the signup page
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SignUpPage()), // Replace with your SignupPage
-                    );
-                  },
-                  child: Text(
-                    'Register',
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.purple.shade800, Colors.black87],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Center(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Campus Placement App',
                     style: TextStyle(
-                      color: Colors.blue, // Color for the Register label
-                      fontSize: 16,
-                      decoration: TextDecoration.underline, // Underline to indicate it's clickable
+                      color: Colors.white,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-              ],
+                  SizedBox(height: 32.0),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Login ID',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.1),
+                      border: Border.all(color: Colors.white70),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Enter your email',
+                        hintStyle: TextStyle(color: Colors.white70),
+                        contentPadding: EdgeInsets.all(12),
+                      ),
+                      style: TextStyle(color: Colors.white),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _email = value!;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 25.0),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Password',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.1),
+                      border: Border.all(color: Colors.white70),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Enter your password',
+                        hintStyle: TextStyle(color: Colors.white70),
+                        contentPadding: EdgeInsets.all(12),
+                      ),
+                      style: TextStyle(color: Colors.white),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _password = value!;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 32.0),
+                  ElevatedButton(
+                    onPressed: _login,
+                    child: Text('Sign In'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueGrey,
+                      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        side: BorderSide(color: Colors.black),
+                      ),
+                      elevation: 5,
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUpPage()),
+                      );
+                    },
+                    child: Text(
+                      'Register',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 16,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
